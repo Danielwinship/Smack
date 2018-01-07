70c54b7e765d09e806422768012ba4e5c00edd35
Daniel Winship <danielwinship@icloud.com>
2018-01-07 16:52:30 -0500

    added new VC files and wired up hamburger button

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/7/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Oulets
+    @IBOutlet weak var menuButton:UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+    }
+
+  
+}
