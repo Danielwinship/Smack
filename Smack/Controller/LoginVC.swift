2ecbe9986b4272dcc94438e1b91f2a541b79d96f
Daniel Winship <danielwinship@icloud.com>
2018-01-09 05:54:56 -0500

    Created loginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/9/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closedPressed (_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
