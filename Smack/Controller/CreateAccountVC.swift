b1e47ccb10444d1962dfa306109c872d13df03f0
Daniel Winship <danielwinship@icloud.com>
2018-01-10 14:16:52 -0500

    created login user function

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -32,7 +32,12 @@ class CreateAccountVC: UIViewController {
         guard let pass = passwordText.text, passwordText.text != "" else {return}
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("registered user!")
+                print("user registered")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("logged in user", AuthService.instance.authToken)
+                    }
+                })
             }
         }
         
--- Smack/Utilites/Constants.swift
+++ Smack/Utilites/Constants.swift
@@ -12,8 +12,9 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 //URL Constants
 //let BASE_URL = "http://localhost:3005/v1"
-let BASE_URL = "https://smacksmacky.herokuapp.com/v1"
-let URL_REGISTER = "\(BASE_URL)/account/register"
+let BASE_URL = "https://smacksmacky.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 
 
@@ -27,3 +28,8 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
