1b33f49bcf766584b893390b7da3cd5e0ab39cc7
Daniel Winship <danielwinship@icloud.com>
2018-01-11 06:20:59 -0500

    completed wiring up avatar picking process

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/11/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+}
