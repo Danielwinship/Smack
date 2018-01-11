4f20f7da14b308ef6853127d669814639ade90e4
Daniel Winship <danielwinship@icloud.com>
2018-01-10 20:10:30 -0500

    completed wiring up avatar picker

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -27,6 +27,14 @@ class CreateAccountVC: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+        
+    }
 
     @IBAction func closePressed(_ sender:Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
--- Smack/View/AvatarCell.swift
+++ Smack/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
  
     @IBOutlet weak var avatarImage: UIImageView!
@@ -15,8 +20,18 @@ class AvatarCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         setupView()
+        
     }
     
+    func configureCell(index:Int, type:AvatarType) {
+        if type == AvatarType.dark {
+            avatarImage.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImage.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+    }
     
     func setupView() {
         self.layer.backgroundColor = UIColor.lightGray.cgColor
@@ -24,6 +39,8 @@ class AvatarCell: UICollectionViewCell {
         self.clipsToBounds = true
         
     }
+    
+   
     
 
 }
