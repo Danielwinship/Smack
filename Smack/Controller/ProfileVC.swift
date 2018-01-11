cdb9b180c67ebba737246e25780c76831e0780f9
Daniel Winship <danielwinship@icloud.com>
2018-01-11 15:55:33 -0500

    created ProfilVC

---
+++ Smack/Controller/ProfileVC.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileVC.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/11/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
