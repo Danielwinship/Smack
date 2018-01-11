20f14a899a2693f2806fad9cbf4eb965b9a14a93
Daniel Winship <danielwinship@icloud.com>
2018-01-11 11:49:05 -0500

    created returnUIColor func

--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -34,7 +34,32 @@ class UserDataService {
     
     func returnUIColor(components: String) -> UIColor {
         let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
         
+        var r, g, b, a : NSString?
+        
+         scanner.scanUpToCharacters(from: comma, into: &r)
+         scanner.scanUpToCharacters(from: comma, into: &g)
+         scanner.scanUpToCharacters(from: comma, into: &b)
+         scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let deaultColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else {return deaultColor}
+        guard let gUnwrapped = g else {return deaultColor}
+        guard let bUnwrapped = b else {return deaultColor}
+        guard let aUnwrapped = a else {return deaultColor}
+        
+        let rFloat = CGFloat(rUnwrapped.doubleValue)
+        let gFloat = CGFloat(gUnwrapped.doubleValue)
+        let bFloat = CGFloat(bUnwrapped.doubleValue)
+        let aFloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rFloat, green: gFloat, blue: bFloat, alpha: aFloat)
+        
+        return newUIColor
         
     }
     
