ca1dccf5d43330af0e6034b3c718748967890880
Daniel Winship <danielwinship@icloud.com>
2018-01-12 16:15:21 -0500

    Creates channel model

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/12/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id:String!
+}
---
+++ Smack/Services/MessageService.swift
@@ -0,0 +1,15 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by Daniel Winship on 1/12/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import Foundation
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+}
