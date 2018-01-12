bac60c979aafd19795633f5b0a2c06022e0aaf2f
Daniel Winship <danielwinship@icloud.com>
2018-01-12 06:12:11 -0500

    fixes problem where user data wasn't loading after re-opening app

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -19,9 +19,13 @@ class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupUserInfo()
     }
 
     @IBAction func loginButtonPressed(_ sender:Any) {
@@ -37,16 +41,20 @@ self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 6
     }
     
    @objc func userDataDidChange(_ notif: Notification){
-    if AuthService.instance.isLoggedIn {
-        loginButton.setTitle(UserDataService.instance.name, for: .normal)
-        userImage.image = UIImage(named: UserDataService.instance.avatarName)
-        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
-    } else {
-        loginButton.setTitle("Login", for: .normal)
-        userImage.image = UIImage(named: "menuProfileIcon")
-        userImage.backgroundColor = UIColor.clear
-        
-    }
+     setupUserInfo()
   }
+    
+    func setupUserInfo() {
+        if AuthService.instance.isLoggedIn {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+            
+        }
+    }
 
 }
