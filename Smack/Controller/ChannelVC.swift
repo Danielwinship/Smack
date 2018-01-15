b7e97954ca8a9f79a8b854e4eb4820759e91cf9d
Daniel Winship <danielwinship@icloud.com>
2018-01-15 09:54:59 -0500

    creates channelVC reload data func and several misc improvements

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -15,13 +15,13 @@ class ChannelVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     //Outlet
     @IBOutlet weak var loginButton:UIButton!
     @IBOutlet weak var userImage:UIImageView!
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.channelsLoaded(_:)), name: NOTIF_CHANNELS_LOADED, object: nil)
         SocketService.instance.getChannel { (success) in
             if success {
                 self.tableView.reloadData()
@@ -38,10 +38,11 @@ class ChannelVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     }
 
     @IBAction func addChannelButtonPressed(_ sender: Any) {
-        let addChannel = AddChannelVC()
-        addChannel.modalPresentationStyle = .custom
-         present(addChannel, animated: true, completion: nil)
-        
+        if AuthService.instance.isLoggedIn {
+            let addChannel = AddChannelVC()
+            addChannel.modalPresentationStyle = .custom
+            present(addChannel, animated: true, completion: nil)
+        }
     }
     
     
@@ -58,9 +59,13 @@ class ChannelVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
        
     }
     
-   @objc func userDataDidChange(_ notif: Notification){
+   @objc func userDataDidChange(_ notif: Notification) {
      setupUserInfo()
   }
+    
+    @objc func channelsLoaded(_ notif:Notification) {
+        tableView.reloadData()
+    }
     
     func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
@@ -71,6 +76,7 @@ class ChannelVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
             loginButton.setTitle("Login", for: .normal)
             userImage.image = UIImage(named: "menuProfileIcon")
             userImage.backgroundColor = UIColor.clear
+            tableView.reloadData()
             
         }
     }
@@ -91,6 +97,14 @@ class ChannelVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let channel = MessageService.instance.channels[indexPath.row]
+        MessageService.instance.selectedChannel = channel
+        NotificationCenter.default.post(name: NOTIF_CHANNELS_SELECTED, object: nil)
+        self.revealViewController().revealToggle(animated: true)
+        
     }
     
     
--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -12,12 +12,15 @@ class ChatVC: UIViewController {
     
     //Oulets
     @IBOutlet weak var menuButton:UIButton!
-
+    @IBOutlet weak var chatNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
         
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail(completion: { (success) in
@@ -28,6 +31,26 @@ class ChatVC: UIViewController {
             
         }
     }
+    
+    
+    @objc func userDataDidChange(_ notif: Notification){
+        if AuthService.instance.isLoggedIn {
+         onLoginGetMessages()
+        } else {
+            chatNameLabel.text = "Please Log In"
+        }
+        
+        
+    }
+    
+    func onLoginGetMessages() {
+        MessageService.instance.findAllChannel { (success) in
+            if success {
+                //do stuff with channels
+            }
+        }
+    }
+    
 
   
 }
