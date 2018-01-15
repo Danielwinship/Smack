66830b7583d181d23cff59062d08d24eb9234b60
Daniel Winship <danielwinship@icloud.com>
2018-01-15 13:55:50 -0500

    creates message model and func to find all messages

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,6 +15,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var messages = [Message]()
     var selectedChannel: Channel?
     
     
@@ -51,12 +52,56 @@ class MessageService {
         }
     
     
+    func findAllMessagesForChannel(channelID: String, completion: @escaping CompletionHandler) {
+             Alamofire.request(URL_GET_MESSAGES, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+                if response.result.error == nil {
+                    self.clearMessages()
+                    guard let data = response.data else {return}
+                    do {
+                        if let json = try JSON(data: data).array {
+                            for item in json {
+                                let messageBody = item["messageBody"].stringValue
+                                let channelID = item["channelID"].stringValue
+                                let id = item["_id"].stringValue
+                                let userName = item["userName"].stringValue
+                                let userAvatar = item["userAvatar"].stringValue
+                                let userAvatarColor = item["userAvatarColor"].stringValue
+                                let timeStamp = item["timeStampe"].stringValue
+                                
+                                let message = Message(message: messageBody, userName: userName, channelID: channelID, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                                
+                                self.messages.append(message)
+                                completion(true)
+
+                            }
+                        }
+                    } catch {
+                        completion(false)
+                        debugPrint(error)
+                    }
+                    
+                }else {
+                    debugPrint(response.result.error as Any)
+                    completion(false)
+                }
+        }
+    }
     
+    
+    
+    func clearMessages() {
+        messages.removeAll()
+    }
     
     func clearChannels() {
         channels.removeAll()
     }
-        
+    
+    
+    
+    
+    
+    
     }
     
 
